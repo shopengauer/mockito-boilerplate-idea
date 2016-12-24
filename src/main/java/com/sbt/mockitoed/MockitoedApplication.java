50a3d11052a7c612429a7cd9429e2bf489f71478
root <localducks@gmail.com>
2016-12-24 23:04:29 +0300

    Add vertx

--- src/main/java/com/sbt/mockitoed/MockitoedApplication.java
+++ src/main/java/com/sbt/mockitoed/MockitoedApplication.java
@@ -1,10 +1,30 @@
 package com.sbt.mockitoed;
 
+import com.sbt.mockitoed.verticles.ServerVerticle;
+import io.vertx.core.Vertx;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MockitoedApplication {
+
+
+	private final ServerVerticle serverVerticle;
+
+	@Autowired
+	public MockitoedApplication(ServerVerticle serverVerticle) {
+		this.serverVerticle = serverVerticle;
+	}
+
+	@PostConstruct
+	private void initVerticles(){
+		Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle(serverVerticle);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MockitoedApplication.class, args);
---
+++ src/main/java/com/sbt/mockitoed/verticles/ServerVerticle.java
@@ -0,0 +1,51 @@
+package com.sbt.mockitoed.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by vasiliy on 24.12.16.
+ */
+@Component
+public class ServerVerticle extends AbstractVerticle{
+
+    @Override
+    public void start() throws Exception {
+        super.start();
+        // To simplify the development of the web components we use a Router to route all HTTP requests
+        // to organize our code in a reusable way.
+        final Router router = Router.router(vertx);
+
+        // In order to use a Thymeleaf template we first need to create an engine
+         final ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
+
+        // Entry point to the application, this will render a custom JADE template.
+
+        router.route().path("/index").handler(StaticHandler.create());
+
+        router.get("/th").handler(ctx -> {
+            // we define a hardcoded title for our application
+            ctx.put("key", "Hi there!");
+
+            // and now delegate to the engine to render it.
+            engine.render(ctx, "templates/index.html", res -> {
+                if (res.succeeded()) {
+                    ctx.response().end(res.result());
+                } else {
+                    ctx.fail(res.cause());
+                }
+            });
+        });
+
+        // start a HTTP web server on port 8080
+        vertx.createHttpServer().requestHandler(router::accept).listen(8081);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+}
